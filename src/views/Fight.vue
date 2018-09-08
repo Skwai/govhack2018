11b94609d8a12764974bbcd8c638a0f1aee9b851
William Bowling <will@wbowling.info>
2018-09-08 16:24:40 +1000

    start of fight

--- src/views/Fight.vue
+++ src/views/Fight.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="FightView">
     <AppModal>
-      <h2>Fight</h2>
+      <h2>{{mob.name}}</h2>
+      <AppBtnGroup>
+        <AppBtn @click.prevent="punch">Punch</AppBtn>
+        <AppBtn @click.prevent="kick">Kick</AppBtn>
+        <AppBtn @click.prevent="slap">Slap</AppBtn>
+      </AppBtnGroup>
     </AppModal>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import AppModal from '@/components/AppModal.vue'
+import AppModal from '@/components/AppModal.vue';
 import Mob from '@/models/Mob';
+import AppBtnGroup from '@/components/AppBtnGroup.vue';
+import AppBtn from '@/components/AppBtn.vue';
 
 @Component({
   components: {
-    AppModal
+    AppModal,
+    AppBtnGroup,
+    AppBtn
   }
 })
 export default class FightView extends Vue {
+  get mob(): Mob {
+    return this.$store.getters['game/currentMob'];
+  }
 }
 </script>
+
+<style lang="stylus" scoped>
+.FightView {
+  text-align: center;
+}
+</style>
